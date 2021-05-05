5016acce5ac639a1e4c976eb7a2d85f4b03c5225
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-05 09:55:06 -0400

    Updated to 1.16.5

---
+++ src/main/java/zeta/zetamod/mod/ForgeBlock.java
@@ -0,0 +1,10 @@
+package zeta.zetamod.mod;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+
+public class ForgeBlock extends Block {
+    public ForgeBlock(Properties properties) {
+        super(properties);
+    }
+}
